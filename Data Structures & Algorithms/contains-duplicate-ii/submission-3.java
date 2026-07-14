068562d285d92a41b459bbcdd9270a1112db08db
saxenaankur444 <saxenaankur444@gmail.com>
2026-07-14 21:07:42 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+		HashSet<Integer> set = new HashSet<>();
+		int l = 0;
+		for (int r = 0; r < nums.length; r++) {
+			if (r - l > k) {
+				set.remove(nums[l++]);
+			}
+			if (set.contains(nums[r]))
+				return true;
+			set.add(nums[r]);
+		}
+		return false;
+	}
+}
