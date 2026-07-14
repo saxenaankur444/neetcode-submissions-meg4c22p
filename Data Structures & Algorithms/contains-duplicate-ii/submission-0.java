525e3df79e4e1b195770a3cf67da5ac0ad4a854a
saxenaankur444 <saxenaankur444@gmail.com>
2026-07-14 19:15:59 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (j - i <= k && nums[j] == nums[i])
+					return true;
+			}
+		}
+		return false;
+	
+    }
+}
