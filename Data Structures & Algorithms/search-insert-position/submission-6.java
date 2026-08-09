fb41af3203490b08bec05a86940b2b3600c695be
saxenaankur444 <saxenaankur444@gmail.com>
2026-08-10 01:41:04 +0530

    Add: search-insert-position - submission-6

---
+++ Data Structures & Algorithms/search-insert-position/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int mid = 0;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            } else
+                r = mid - 1;
+        }
+        return l;
+    }
+}
