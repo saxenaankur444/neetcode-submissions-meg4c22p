39c1eba44a9d91c0ff8779f553a92b93dcc4fe16
saxenaankur444 <saxenaankur444@gmail.com>
2026-08-22 13:50:08 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+		int maxJump = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (maxJump < i)
+				return false;
+			maxJump = Math.max(maxJump, i + nums[i]);
+			if (maxJump >= nums.length - 1)
+				return true;
+		}
+		return true;
+	}
+}
