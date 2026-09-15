8c811ff79c4747499a71ed38ff91a6425bd7e02e
saxenaankur444 <saxenaankur444@gmail.com>
2026-09-16 01:54:57 +0530

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+		int mr = 0;
+		int count = 0;
+		int cur = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			mr = Math.max(mr, i + nums[i]);
+			if (i == cur) {
+				count++;
+				cur = mr;
+			}
+		}
+		return count;
+	}
+}
