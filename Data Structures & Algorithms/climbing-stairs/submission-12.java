b2ba2219dc3c8c905f38149fcdca2e26a649feb0
saxenaankur444 <saxenaankur444@gmail.com>
2026-09-21 13:21:38 +0530

    Add: climbing-stairs - submission-12

---
+++ Data Structures & Algorithms/climbing-stairs/submission-12.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+		if (n <= 2) {
+			return n;
+		}
+		int[] d = new int[n + 1];
+		d[0] = 0;
+		d[1] = 1;
+		d[2] = 2;
+		for (int j = 3; j <= n; j++) {
+			d[j] = d[j - 1] + d[j - 2];
+		}
+
+		return d[n];
+	 }
+}
